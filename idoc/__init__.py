44b3882d40608d39ba01b1383de71a8dca498d17
Marine Schimel <marine.schimel@hotmail.fr>
2022-03-11 18:10:54 +0000

    update init

--- idoc/__init__.py
+++ idoc/__init__.py
@@ -1,3 +1,3 @@
 from . import ilqr, tilqr, lqr, qp, sqp, typs, utils
 
-__all__ = ["ilqr", "tilqr", "lqr", "qp", "sqp", "typs", "utils"]
+__all__ = ["ilqr", "tilqr", "lqr", "qp", "sqp", "typs", "batch_lqr", "batch_ilqr", "utils"]
